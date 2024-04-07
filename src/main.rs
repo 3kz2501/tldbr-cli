a57ef6b5d178fcdcef0536901dc80c2db58e6bf6
nilnull <nilnull@pop-os.localdomain>
2024-04-07 21:12:12 +0900

    remove unused

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, Write};
-use futures::stream::{self, StreamExt};
+use futures::stream::{StreamExt};
 use reqwest::Client;
 use serde_json::json;
 
